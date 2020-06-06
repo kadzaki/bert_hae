9fb1e2d17faa37f4b54836939ab4e7e2cad77f93
Zakaria <kadzaki@gmail.com>
2020-06-06 22:30:20 +0000

    change max seq length

--- doqa_flags.py
+++ doqa_flags.py
@@ -54,7 +54,7 @@ flags.DEFINE_bool(
     "models and False for cased models.")
 
 flags.DEFINE_integer(
-    "max_seq_length", 128,
+    "max_seq_length", 384,
     "The maximum total input sequence length after WordPiece tokenization. "
     "Sequences longer than this will be truncated, and sequences shorter "
     "than this will be padded.")
